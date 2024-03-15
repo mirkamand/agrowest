f476a0576e42a55247550eb05dcae2db321e0614
Mirkamand Seyidli <mirkemend97@gmail.com>
2024-03-15 17:55:20 +0400

    Modified RootLayout cmp for scrolling issue

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import MainNavbar from "./MainNavbar";
 import HeaderSlider from "./HeaderSlider";
 import Footer from "./Footer";
 
 const RootLayout = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <>
       <MainNavbar />
